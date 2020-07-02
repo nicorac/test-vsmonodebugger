98ccf4b687a31c6e1025fa2a59d8d55716629385
nicorac <coolsoft.ita@gmail.com>
2020-07-02 21:19:13 +0200

    Add project files

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MonoDebuggerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if X86
+            Console.WriteLine("X86");
+#elif ANYCPU
+            Console.WriteLine("AnyCPU");
+#else
+            Console.WriteLine("None");
+#endif
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# test-vsmonodebugger
+Test project to help fixing this VSMonoDebugger issue:
+
+https://github.com/GordianDotNet/VSMonoDebugger/issues/14
